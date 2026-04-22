f34d091084edf69b7503cfb1a65f7fcf516d4c15
MarsoL4 <enzogmarsola@gmail.com>
2026-04-22 14:06:41 -0300

    refatora gerenciamento de usuários, implementando camada de casos de uso e mapeamento de entidades

--- clean-arch/descomplicando-clean-architecture-java/src/main/java/br/com/alura/codechella/application/usecases/ListarUsuarios.java
+++ clean-arch/descomplicando-clean-architecture-java/src/main/java/br/com/alura/codechella/application/usecases/ListarUsuarios.java
@@ -3,14 +3,17 @@ package br.com.alura.codechella.application.usecases;
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
 
-public class CriarUsuario {
+import java.util.List;
+
+public class ListarUsuarios {
+
     private final RepositorioDeUsuario repositorio;
 
-    public CriarUsuario(RepositorioDeUsuario repositorio) {
+    public ListarUsuarios(RepositorioDeUsuario repositorio) {
         this.repositorio = repositorio;
     }
 
-    public Usuario cadastrarUsuario(Usuario usuario) {
-        return repositorio.cadastrarUsuario(usuario);
+    public List<Usuario> obterTodosUsuarios(){
+        return this.repositorio.listarTodos();
     }
 }
--- clean-arch/descomplicando-clean-architecture-java/src/main/java/br/com/alura/codechella/infra/persistence/UsuarioRepository.java
+++ clean-arch/descomplicando-clean-architecture-java/src/main/java/br/com/alura/codechella/infra/persistence/UsuarioRepository.java
@@ -3,6 +3,4 @@ package br.com.alura.codechella.infra.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
-
-
 }
